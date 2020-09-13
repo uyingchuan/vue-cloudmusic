152491354f0871d083c84b924d29df34fecf38bd
u影川 <uyingchuan@163.com>
2020-09-13 22:42:36 +0800

    build:优化项目打包

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/cloudmusic' : '/',
+    outputDir: 'music',
+    configureWebpack: {
+        devtool: isProd ? false : 'source-map'
+    },
     // 配置css加载额外的sass的全局变量
     css: {
         loaderOptions: {
