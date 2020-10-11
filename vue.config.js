fc1f60f12ade54e85719c68efa729626406f8849
u影川 <uyingchuan@163.com>
2020-10-11 21:13:37 +0800

    chore

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,25 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
     outputDir: 'music',
     configureWebpack: {
-        devtool: isProd ? false : 'source-map'
+        devtool: isProd ? false : 'source-map',
+        devServer: {
+            open: true,
+            proxy: {
+                '/netease-api': {
+                    target: 'http://localhost:3000',
+                    pathRewrite: { '^/netease-api': '' },
+                    changeOrigin: true,
+                    secure: false
+                }
+            },
+            prot: 8888
+        },
+        externals: isProd ? {
+            vue: 'Vue',
+            'vue-router': 'VueRouter',
+            vuex: 'Vuex',
+            axios: 'axios'
+        } : {}
     },
     // 配置预加载sass的全局变量
     css: {
