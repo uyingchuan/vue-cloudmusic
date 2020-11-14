8df7794c06c686203febd8113bcc5fb181bce789
u影川 <uyingchuan@163.com>
2020-11-14 14:23:38 +0800

    perf:dns-prefetch

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import { Loading } from 'element-ui'
 import store from '@/store'
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'http://mapi.levons.cn'
 
 // 不带全局loading的请求实例
 export const requestWithoutLoading = createBaseInstance() 
